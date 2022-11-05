928273998cc2418bee51c04310d23da88543cf6a
SardarBadar <dataentrybadar@gmail.com>
2022-11-05 21:30:11 +0500

    scraping

--- taneps.php
+++ taneps.php
@@ -56,7 +56,8 @@ class TanepsAwardModel
 }
 
 $option = [
-    'headers' => [
+    'timeout' => 300,
+    // 'headers' => [
         'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0',
         'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language' => 'en-US,en;q=0.5',
@@ -68,15 +69,15 @@ $option = [
         'Sec-GPC' => '1',
         'Cache-Control' => 'max-age=0, no-cache',
         'Pragma' => 'no-cache'
-    ],
+    // ],
 ];
 
-$tanepsAwardModel = array(['Tender No', 'Tender Url', 'Procuring Entity', 'Supplier Name', 'Award Date', 'Award Amount']);
 
 $client = new Client();
 // $client->request('');
 
-for ($x = 1; $x <= 10; $x++) {
+for ($x = 1; $x <= 539; $x++) {
+$tanepsAwardModel = array(['Tender No', 'Tender Url', 'Procuring Entity', 'Supplier Name', 'Award Date', 'Award Amount']);
 echo "The page number is: $x";
 
 // $client = new GuzzleHttp\Client();
@@ -86,64 +87,69 @@ $procuringEntity=[];
 $supplierName=[];
 $awardDate=[];
 $awardAmount=[];
-$url="http://www.taneps.go.tz/epps/viewAllAwardedContracts.do?d-3998960-p={$x}&selectedItem=viewAllAwardedContracts.do&T01_ps=100";
+$url="http://www.taneps.go.tz/epps/viewAllAwardedContracts.do?d-3998960-p=$x&selectedItem=viewAllAwardedContracts.do&T01_ps=100";
 echo "\nurl :".$url."\n";
-$res = $client->request('GET', $url, $option);
-echo "\nThe response got it is: $x";
+try {
 
- $res->filter('#T01 tbody tr td a')->each(function ($node,$i) use(&$tenderNumbers,&$tenderLinks)  {
-    static $j=1;
-    if($i%2==0){
-    echo "\n{$j} {$node->text()}";
-    echo 'https://www.taneps.go.tz'.$node->attr('href');
+    $res = $client->request('GET', $url, $option);
+    echo "\nThe response got it is: $x";
+    
+    $res->filter('#T01 tbody tr td a')->each(function ($node,$i) use(&$tenderNumbers,&$tenderLinks)  {
+        static $j=1;
+        if($i%2==0){
+            echo "\n{$j} {$node->text()}";
+            echo 'https://www.taneps.go.tz'.$node->attr('href');
     $tenderNumbers[]=$node->text();
     $tenderLinks[]= 'https://www.taneps.go.tz'.$node->attr('href');
     $j++;
-    }
-    });
+}
+});
 
-    $res->filter('#T01 tbody tr td:nth-child(2)')->each(function ($node,$i) use(&$procuringEntity)  {
-        static $j=1;
+$res->filter('#T01 tbody tr td:nth-child(2)')->each(function ($node,$i) use(&$procuringEntity)  {
+    static $j=1;
         echo "\n{$j} {$node->text()}";
         $procuringEntity[]=$node->text();
         $j++;
-        });
-
+    });
+    
     $res->filter('#T01 tbody tr td:nth-child(3)')->each(function ($node,$i) use(&$supplierName)  {
             static $j=1;
             echo "\n{$j} {$node->text()}";
             $supplierName[]=$node->text();
             $j++;
             });  
-
-    $res->filter('#T01 tbody tr td:nth-child(4)')->each(function ($node,$i) use(&$awardDate)  {
+            
+            $res->filter('#T01 tbody tr td:nth-child(4)')->each(function ($node,$i) use(&$awardDate)  {
                 static $j=1;
                 echo "\n{$j} {$node->text()}";
                 $awardDate[]=$node->text();
                 $j++;
-                });  
-    $res->filter('#T01 tbody tr td:nth-child(5)')->each(function ($node,$i) use(&$awardAmount)  {
-                    static $j=1;
-                    echo "\n{$j} {$node->text()}";
-                    $awardAmount[]=$node->text();
-                    $j++;
-                    });  
-
+            });  
+            $res->filter('#T01 tbody tr td:nth-child(5)')->each(function ($node,$i) use(&$awardAmount)  {
+                static $j=1;
+                echo "\n{$j} {$node->text()}";
+                $awardAmount[]=$node->text();
+                $j++;
+            });  
+            
                 
-                    echo "\nThe starting push array $x";
-
-                
-                    for($k=0;$k<sizeof($awardDate);$k++){
-                  array_push($tanepsAwardModel,[$tenderNumbers[$k], $tenderLinks[$k], $procuringEntity[$k], $supplierName[$k], $awardDate[$k], $awardAmount[$k]]);
-                    }
-                    // Open a file in write mode ('w')
-                    echo "\nThe starting csv $x";
-
-                    if($i==1)
-                    $fp = fopen('site.csv', 'w');
-                    else{
-                    $fp= fopen('site.csv', 'a');
-                    }
+            echo "\nThe starting push array $x";
+            
+            
+            for($k=0;$k<sizeof($awardDate);$k++){
+                array_push($tanepsAwardModel,[$tenderNumbers[$k], $tenderLinks[$k], $procuringEntity[$k], $supplierName[$k], $awardDate[$k], $awardAmount[$k]]);
+            }
+        } catch (\Exception $e) {
+            print_r("Error timeout");
+        }
+            // Open a file in write mode ('w')
+            echo "\nThe starting csv $x";
+            
+            // if($i==1)
+                    $fp = fopen("page-{$x}.csv", 'w');
+                    // else{
+                    // $fp= fopen('site.csv', 'a');
+                    // }
                       
                     // Loop through file pointer and a line
                     foreach ($tanepsAwardModel as $fields) {
@@ -153,5 +159,5 @@ echo "\nThe response got it is: $x";
                     fclose($fp);
                     $tanepsAwardModel=[];
                     echo "\nEnd 100 $x\n";
-
+                    sleep(15);
                 } 
